9c018c1ad011ad792ea0df8a8056eb81b8a3383d
nab0310 <nabehrens@sbcglobal.net>
2017-10-21 13:55:49 -0500

    Adding the path package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
+var path = require('path');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
