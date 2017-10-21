3f1d1c5643123d7203ebef3b56729409ca3ebaf9
nab0310 <nabehrens@sbcglobal.net>
2017-10-20 20:03:29 -0500

    Added heroku port env support

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(pprt, function () {
   console.log('Example app listening on port 3000!')
 })
