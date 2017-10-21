852b7954a14c9d79d62abce3abf1d2764b67380d
nab0310 <nabehrens@sbcglobal.net>
2017-10-21 13:51:25 -0500

    changing index file to serve html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('./index.html')
+  res.send(./index.html)
 })
 
 app.listen(port, function () {
