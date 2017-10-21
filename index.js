277272bad73c8a62c5bd6d0d1c7063ccc7e66f48
AustinKerr42 <akerr@iastate.edu>
2017-10-21 14:51:54 -0500

    Fixing routing

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+app.get('/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/about.html'));
+})
+
+app.get('/tweet.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/tweet.html'));
+})
+
+app.get('/polls.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/polls.html'));
+})
+
+app.get('/contact.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/contact.html'));
+})
+
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
